796773bce04354c221e1e956c465dc662c02caa2
Brett Mack <brett.mack@contino.io>
2018-03-08 08:52:46 +0000

    powerline

---
+++ .gopowerline.zsh
@@ -0,0 +1,16 @@
+function powerline_precmd() {
+    PS1="$(/usr/local/bin/powerline-go -error $? -shell zsh -cwd-max-depth 4 -modules "cwd,perms,git,hg,jobs,root")"
+}
+
+function install_powerline_precmd() {
+  for s in "${precmd_functions[@]}"; do
+    if [ "$s" = "powerline_precmd" ]; then
+      return
+    fi
+  done
+  precmd_functions+=(powerline_precmd)
+}
+
+if [ "$TERM" != "linux" ]; then
+    install_powerline_precmd
+fi
